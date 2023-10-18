278b87dbe0de87cd6787553de747c4533ebd40b0
haitran <hai@entest.io>
2023-10-18 11:28:49 +0000

    test

--- config.ts
+++ config.ts
@@ -1,16 +1,16 @@
 export const config = {
-  USERNAME: "64585428-4011-7037-260c-b07c449d8aa3",
+  USERNAME: "6a7533f5-ea77-462e-a8b0-07d34a1c238b",
   PASSWORD: "Demo@2023",
-  USER_POOL_ID: "us-east-1_ICeo5d5SV",
-  COGNITO_POOL_ID: "cognito-idp.us-east-1.amazonaws.com/us-east-1_ICeo5d5SV",
-  IDENTITY_POOL_ID: "us-east-1:408ba96a-d967-4f0d-abdd-ca59a79d158b",
-  CLIENT_ID: "4ejpk2vjlkjl7t7qirtqqiui1",
+  USER_POOL_ID: "us-east-1_WaJGNPw9n",
+  COGNITO_POOL_ID: "cognito-idp.us-east-1.amazonaws.com/us-east-1_WaJGNPw9n",
+  IDENTITY_POOL_ID: "us-east-1:c56834e1-fdd4-47df-9eda-185552dbe53d",
+  CLIENT_ID: "7cengd3n2ldaegjvble8isiu9p",
   REGION: "us-east-1",
-  BUCKET: "cognito-demo-bucket-251285454800-1",
+  BUCKET: "cognito-demo-bucket-315305460736-1",
   API_URL_IMAGE:
-    "https://jnzvcd82ic.execute-api.us-east-1.amazonaws.com/prod/iamge",
+    "https://9w82qhrfkg.execute-api.us-east-1.amazonaws.com/prod/iamge",
   API_URL_MESSAGE:
-    "https://jnzvcd82ic.execute-api.us-east-1.amazonaws.com/prod/message",
+    "https://9w82qhrfkg.execute-api.us-east-1.amazonaws.com/prod/message",
   API_URL_POLLY:
-    "https://jnzvcd82ic.execute-api.us-east-1.amazonaws.com/prod/polly",
+    "https://9w82qhrfkg.execute-api.us-east-1.amazonaws.com/prod/polly",
 };
